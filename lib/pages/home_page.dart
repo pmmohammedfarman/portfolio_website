0bb04bfbac286f417fb314e84892084d4d42bea2
pmmohammedfarman <mohammedfarman04@gmail.com>
2024-08-30 12:18:49 +0530

    part 3 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/constants/colors.dart';
 import 'package:portfolio_website/constants/nav_items.dart';
+import 'package:portfolio_website/widgets/header_desktop.dart';
+import 'package:portfolio_website/widgets/site_logo.dart';
+
+import '../styles/theme.dart';
+import '../widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,40 +23,11 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.vertical,
           children: [
             //Main
-            Container(
-              height: 60,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Colors.transparent, CustomColor.bgLight1]),
-                  borderRadius: BorderRadius.circular(100)),
-              child: Row(
-                children: [
-                  Text(
-                    "P M Mohammed Farman",
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                        color: CustomColor.yellowSecondary),
-                  ),
-                  Spacer(),
-                  for (int i = 0; i < navTitles.length; i++)
-                    Padding(
-                      padding: const EdgeInsets.only(right : 20.0),
-                      child: TextButton(onPressed: () {}, child: Text("${navTitles[i]}",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight:   FontWeight.w500,
-                        color: CustomColor.whitePrimary
-                      ),
-                      )),
-                    )
-                ],
-              ),
+            //HeaderDesktop(),
+            HeaderMobile(
+              onMenuTap: () {},
+              onLogoTap: () {},
             ),
-
             //Skills
             Container(
               height: 500,
